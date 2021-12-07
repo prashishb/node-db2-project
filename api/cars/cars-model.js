c50a48b7bedd39b0c1fe25c9d602ef5126a810b4
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-07 14:44:13 -0500

    cars-model.js completed

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -4,12 +4,13 @@ const getAll = () => {
   return db('cars');
 };
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = (id) => {
+  return db('cars').where({ id }).first();
 };
 
-const create = () => {
-  // DO YOUR MAGIC
+const create = async (car) => {
+  const [id] = await db('cars').insert(car);
+  return getById(id);
 };
 
 module.exports = {
