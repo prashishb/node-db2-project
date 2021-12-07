0a297b2203485ffddcd0c149aec5e437977bd22e
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-07 14:25:52 -0500

    server initial setups

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,21 @@
-// DO YOUR MAGIC
+const express = require('express');
+const Car = require('./cars-model');
+
+const router = express.Router();
+
+// [GET] /api/cars
+router.get('/', async (req, res, next) => {
+  res.json('Get all cars');
+});
+
+// [GET] /api/cars/:id
+router.get('/:id', async (req, res, next) => {
+  res.json('Get car by id');
+});
+
+// [POST] /api/cars
+router.post('/', async (req, res, next) => {
+  res.json('Post new car');
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,17 @@
-const express = require("express")
+const express = require('express');
+const carsRouter = require('./cars/cars-router');
 
-const server = express()
+const server = express();
 
-// DO YOUR MAGIC
+server.use(express.json());
+server.use('/api/cars', carsRouter);
 
-module.exports = server
+//eslint-disable-next-line
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
